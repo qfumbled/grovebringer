6b33a385dd7d95446b82bf03965fb85f2aab5e78
chimeri <chimeridum@proton.me>
2026-01-02 00:47:22 -0500

    Fix home-manager imports conflict

--- modules/home/opt/default.nix
+++ modules/home/opt/default.nix
@@ -7,39 +7,10 @@
 }:
 
 {
-  imports = [
-    ./programs
-    ./shells
-    ./terminals
-    ./wm
-    ./style
-  ];
-
-  # Only import modules that are enabled
-  config = {
-    # Programs module
-    imports = lib.optionals config.grovebringer.home.programs.enable [
-      ./programs
-    ];
-
-    # Shells module
-    imports = lib.optionals config.grovebringer.home.shells.enable [
-      ./shells
-    ];
-
-    # Terminals module
-    imports = lib.optionals config.grovebringer.home.terminals.enable [
-      ./terminals
-    ];
-
-    # Window manager module
-    imports = lib.optionals config.grovebringer.home.wm.enable [
-      ./wm
-    ];
-
-    # Style module (always imported for theming)
-    imports = [
-      ./style
-    ];
-  };
+  imports = 
+    (lib.optionals config.grovebringer.home.programs.enable [ ./programs ]) ++
+    (lib.optionals config.grovebringer.home.shells.enable [ ./shells ]) ++
+    (lib.optionals config.grovebringer.home.terminals.enable [ ./terminals ]) ++
+    (lib.optionals config.grovebringer.home.wm.enable [ ./wm ]) ++
+    [ ./style ];  # Style always imported for theming
 }
