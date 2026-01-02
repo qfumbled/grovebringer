6e68306d2ca5c8a5e410a6d36f9ba4b7da6ab2e4
chimeri <chimeridum@proton.me>
2026-01-02 00:50:38 -0500

    Create basic hardware configuration for aureliteiron

---
+++ hosts/aureliteiron/hardware.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  # Basic filesystem configuration - adjust as needed
+  fileSystems."/" = 
+    { device = "/dev/disk/by-uuid/CHANGE-THIS-UUID";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd with networkd
+  # you might want to disable this.
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
