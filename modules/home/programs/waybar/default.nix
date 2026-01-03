69708c3219dd619f905536a9578c10a4d1fbc26b
qfumbled <chimeridum@proton.me>
2026-01-03 16:05:48 -0500

    attempt to steal @elythh waybar (older) has been successful

--- modules/home/programs/waybar/default.nix
+++ modules/home/programs/waybar/default.nix
@@ -9,141 +9,246 @@
       settings = [
         {
           layer = "top";
-          position = "bottom";
-          height = 36;
-          spacing = 5;
-          modules-left = ["wlr/taskbar"];
-          modules-right = ["tray" "wireplumber" "battery" "network" "clock"];
-          "clock" = {
+          position = "top";
+          exclusive = true;
+          passthrough = false;
+          fixed-center = true;
+          gtk-layer-shell = true;
+          spacing = 0;
+          name = "hobar";
+          modules-left = [
+            "image"
+            "clock"
+            "hyprland/workspaces"
+            "tray"
+          ];
+          modules-right = [
+            "battery"
+            "memory"
+            "pulseaudio"
+            "clock"
+          ];
+
+          "image" = {
+            path = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            size = 24;
             tooltip = false;
-            interval = 60;
-            format = "{:%a %b %d %H:%M}";
-            max-length = 25;
           };
-          "wlr/taskbar" = {
-            format = "{name}";
-            on-click = "minimize-raise";
+
+          "hyprland/workspaces" = {
+            format = "{id}";
+            active-only = false;
+            format-icons = {
+              "1" = "Q";
+              "2" = "W";
+              "3" = "E";
+              "4" = "R";
+              "5" = "T";
+              "6" = "Y";
+              "7" = "U";
+              "8" = "I";
+              "9" = "O";
+              "10" = "P";
+            };
           };
-          "tray" = {
-            icon-size = 12;
-            spacing = 8;
+
+          "group/backlight-modules" = {
+            modules = [
+              "backlight#icon"
+              "backlight#percent"
+            ];
+            orientation = "inherit";
           };
-          "wireplumber" = {
-            format-icons = ["" "" ""];
+
+          "backlight#icon" = {
             format = "{icon}";
-            format-muted = "󰖁";
-            tooltip-format = "{volume}% | {node_name}";
-            on-click = "${pkgs.pamixer}/bin/pamixer --toggle-mute";
+            format-icons = [
+              "󰃞"
+              "󰃟"
+              "󰃠"
+            ];
+            on-scroll-up = "${pkgs.brightnessctl}/bin/brightnessctl set 1%+ &> /dev/null";
+            on-scroll-down = "${pkgs.brightnessctl}/bin/brightnessctl set 1%- &> /dev/null";
+            tooltip-format = "Backlight: {percent}%";
           };
-          "battery" = {
-            interval = 3;
+
+          "backlight#percent" = {
+            format = "{percent}%";
+            tooltip-format = "Backlight: {percent}%";
+          };
+
+          tray = {
+            icon-size = 14;
+            spacing = 14;
+            show-passive-items = true;
+            reverse-direction = true;
+          };
+
+          memory = {
+            interval = 20;
+            format = "MEM: [ {icon} ] <span size='8pt'>{percentage}%</span>";
+            tooltip-format = "MEM_TOT\t: {total}GiB\nSWP_TOT\t: {swapTotal}GiB\n\nMEM_USD\t: {used:0.1f}GiB\nSWP_USD\t: {swapUsed:0.1f}GiB";
+            format-icons = [
+              "░░░░░░░░"
+              "█░░░░░░░"
+              "██░░░░░░"
+              "███░░░░░"
+              "████░░░░"
+              "█████░░░"
+              "██████░░"
+              "<span color='#B7416E'>!!!!!!!!</span>"
+              "<span color='#B7416E'>CRITICAL</span>"
+            ];
+          };
+
+          battery = {
             states = {
-              warning = 30;
+              warning = 20;
               critical = 15;
             };
-            format-icons = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂"];
-            format = "{icon}";
-            format-charging = "{icon} 󰉁";
-            tooltip-format = "{capacity}% | {timeTo}";
-            max-length = 25;
+            format = "BAT= [ {icon} ] <span size='8pt'>{capacity}%</span>";
+            interval = 5;
+            format-charging = "BAT= [ {icon} ] <span size='8pt'>CHRG</span>";
+            format-plugged = "BAT= [ {icon} ] <span size='8pt'>PLUG</span>";
+            tooltip-format = "BATTERY= {power}W\nSTATUS= {timeTo}\nCYCLES= {cycles}\nHEALTH= {health}";
+            format-icons = [
+              "<span color='#B7416E'>CRITICAL"
+              "<span color='#B7416E'>!!!!!!!!</span>"
+              "██░░░░░░"
+              "███░░░░░"
+              "████░░░░"
+              "█████░░░"
+              "██████░░"
+              "███████░"
+              "████████"
+            ];
           };
-          "network" = {
-            format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
-            format = "{ifname}";
-            format-wifi = "{icon}";
-            format-ethernet = "";
-            format-disconnected = "󰤭";
-            tooltip-format = "{ifname} via {gwaddr}";
-            tooltip-format-wifi = "{signalStrength}% | {essid}";
-            tooltip-format-ethernet = "Connected: Wired";
-            tooltip-format-disconnected = "Disconnected";
-            max-length = 50;
+
+          clock = {
+            actions = {
+              on-scroll-down = "shift_down";
+              on-scroll-up = "shift_up";
+            };
+            calendar = {
+              format = {
+                days = "<span color='@theme_text_color'><b>{}</b></span>";
+                months = "<span color='@theme_text_color'><b>{}</b></span>";
+                today = "<span color='@theme_selected_bg_color'><b><u>{}</u></b></span>";
+                weekdays = "<span color='@theme_fg_color'><b>{}</b></span>";
+              };
+              mode = "month";
+              on-scroll = 1;
+            };
+            format = "{:%I:%M %p}";
+            tooltip-format = "{calendar}";
           };
         }
       ];
 
-      style = with config.lib.stylix.colors.withHashtag; ''
-        *:not(separator) {
-          all: unset;
-          font-family: "JetBrains Mono Nerd Font Propo";
-          font-size: 10pt;
+      style = ''
+        * {
+            border: none;
+            font-family: Roboto, RobotoMono Nerd Font;
+            font-weight: bold;
+            font-size: 14px;
+            min-height: 0;
+            padding: 2px;
+            border-radius: 0px;
         }
 
-        /* Main Bar */
         window#waybar {
-          background: ${base00};
-          border-top: 2px solid ${base01};
-          color: ${base05};
-        }
-
-        window#waybar.hidden {
-          opacity: 0.2;
-        }
-
-        .modules-left {
-          margin-left: 0.5rem;
-        }
-
-        .modules-right {
-          margin-right: 0.5rem;
-        }
-
-        menu {
-          background: ${base01};
-          padding: 8px;
-        }
-
-        menu separator {
-          background: ${base03};
-          margin: 0.4rem 0;
-        }
-
-        menu menuitem {
-          color: ${base05};
-          padding: 0.8rem;
-        }
-
-        menu menuitem:hover {
-          background: ${base02};
+            background: @theme_bg_color;
+            color: @theme_fg_color;
         }
 
         tooltip {
-          background: ${base01};
-          color: ${base05};
+            background: @theme_bg_color;
         }
 
-        tooltip label {
-          margin: 0.8rem;
+        #workspaces button {
+            color: @theme_fg_color;
         }
 
+        #workspaces button.active {
+            color: @theme_selected_bg_color;
+            background: @theme_bg_color;
+        }
+
+        #workspaces button.focused {
+            color: @theme_fg_color;
+            background: @theme_bg_color;
+        }
+
+        #workspaces button.urgent {
+            color: @theme_fg_color;
+            background: @theme_bg_color;
+        }
+
+        #workspaces button:hover {
+            background: @theme_fg_color;
+            color: @theme_bg_color;
+        }
+
+        #custom-language,
+        #custom-updates,
+        #custom-caffeine,
+        #custom-weather,
+        #window,
         #clock,
-        #taskbar,
-        #tray,
         #battery,
-        #wireplumber,
+        #pulseaudio,
+        #network,
+        #workspaces,
+        #tray,
+        #cpu,
+        #backlight {
+            background: @theme_base_color;
+            padding: 0px 10px;
+            margin: 0px;
+        }
+
+        #tray {
+            margin-right: 10px;
+        }
+
+        #workspaces {
+            color: @theme_text_color;
+        }
+
+        #window {
+            color: @theme_text_color;
+        }
+
+        #clock {
+            color: @theme_text_color;
+        }
+
         #network {
-          margin: 0 0.2rem;
+            color: @theme_text_color;
         }
 
-        #taskbar button {
-          margin: 0 0.4rem;
+        #pulseaudio {
+            color: @theme_text_color;
         }
 
-        #taskbar:first-child {
-          margin-left: 0;
+        #pulseaudio.microphone {
+            color: @theme_text_color;
         }
 
-        #taskbar:last-child {
-          margin-right: 0;
+        #battery {
+            color: @theme_text_color;
         }
 
-        #taskbar button:hover,
-        #taskbar button.active {
-          font-weight: bold;
+        #battery.warning:not(.charging) {
+        	color: #C4E969;
         }
 
-        #wireplumber.muted {
-          color: ${base08};
+        #battery.critical:not(.charging) {
+        	color: #B7416E;
+        }
+
+        #custom-weather {
+            color: @theme_text_color;
         }
       '';
     };
