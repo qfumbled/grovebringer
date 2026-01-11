1c5d83cd90c44c1eaced95e417719b9928242927
chimeri <chimeridum@proton.me>
2026-01-10 21:19:33 -0500

    Poacher's older KDE laptop configuration

--- modules/home/desktop/plasma/default.nix
+++ modules/home/desktop/plasma/default.nix
@@ -5,23 +5,21 @@
   programs.plasma = {
     enable = true;
     workspace = {
-    wallpaper = toString (./../../../../home/shared/walls/anatomy.webp);
-     iconTheme = "Papirus-Dark";
-     lookAndFeel = "org.kde.breeze.desktop";
-     cursor.theme = "breeze";
+      wallpaper = toString (./../../../../home/shared/walls/anatomy.webp);
+      iconTheme = "Papirus-Dark";
+      lookAndFeel = "org.kde.breeze.desktop";
     };
     shortcuts = {
       "kwin" = {
-        "Window Fullscreen" = "Meta+F";
-        "Window Close" = "Meta+Q";
-        "Switch to Desktop 1" = "Meta+1";
-        "Switch to Desktop 2" = "Meta+2";
-        "Switch to Desktop 3" = "Meta+3";
-        "Switch to Desktop 4" = "Meta+4";
-        "Window to Desktop 1" = "Meta+Shift+1";
-        "Window to Desktop 2" = "Meta+Shift+2";
-        "Window to Desktop 3" = "Meta+Shift+3";
-        "Window to Desktop 4" = "Meta+Shift+4";
+         "Window Fullscreen" = "Meta+F";
+         "Window Close" = "Meta+Q";
+         "Switch to Desktop 1" = "Meta+1";
+         "Switch to Desktop 2" = "Meta+2";
+         "Switch to Desktop 3" = "Meta+3";
+         "Switch to Desktop 4" = "Meta+4";
+         "Window to Desktop 2" = "Meta+Shift+2";
+         "Window to Desktop 3" = "Meta+Shift+3";
+         "Window to Desktop 4" = "Meta+Shift+4"; 
       };
       "plasmashell" = {
         "activate task manager entry 1" = "none";
@@ -45,48 +43,20 @@
     };
     panels = [
       {
-        location = "bottom";
-        height = 36;
-        floating = false;
-        hiding = "none";
-        alignment = "center";
         opacity = "opaque";
+        location = "bottom";
+        height = 30;
+        floating = false;
         widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.panelspacer"
           {
-            name = "org.kde.plasma.kickoff";
-            config.General = {
-              icon = "start-here-kde-symbolic";
+            digitalClock = {
+              date.enable = false;
             };
           }
           "org.kde.plasma.panelspacer"
-          {
-            name = "org.kde.plasma.icontasks";
-            config.General = {
-              iconsOnly = true;
-              groupPopups = false;
-              showOnlyCurrentDesktop = true;
-              launchers = [
-                "applications:firefox.desktop"
-                "applications:org.kde.konsole.desktop"
-                "applications:org.kde.dolphin.desktop"
-              ];
-            };
-          }
-          "org.kde.plasma.panelspacer"
-          {
-            name = "org.kde.plasma.systemtray";
-            config.General.scaleIconsToFit = true;
-          }
-          {
-            name = "org.kde.plasma.digitalclock";
-            config = {
-              Appearance = {
-                showDate = false;
-                showSeconds = false;
-                use24hFormat = false;
-              };
-            };
-          }
+          "org.kde.plasma.systemtray"
         ];
       }
     ];
